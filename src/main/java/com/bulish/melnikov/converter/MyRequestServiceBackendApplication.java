12e5dd1b80cf451dbf7ec318118dc9f04a1421b9
Evgenia Bullis <bulish2015@yandex.ru>
2025-04-17 19:16:23 +0300

    init.

---
+++ src/main/java/com/bulish/melnikov/converter/MyRequestServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bulish.melnikov.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRequestServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRequestServiceBackendApplication.class, args);
+	}
+
+}
